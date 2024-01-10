48b7cf90b17a7ff842ad4a0a672bc9ea8c28206b
syard <syarddauti1@gmail.com>
2024-01-10 23:12:49 +0200

    Revert "..."
    This reverts commit 4c2ca725e7ebb4522238d3ffe1c291b35aa6ac48.

--- front/src/script/script.js
+++ front/src/script/script.js
@@ -4,21 +4,16 @@ const img = document.querySelector(".div-img")
 const section = document.querySelector("section");
 const btn = document.getElementById("check")
 // btn.checked =true
-const mainImg = document.querySelector(".div-img")
-console.log(mainImg)
 const labelBtn = document.querySelector(".checkbtn")
 const logBtn = document.getElementById("login") //login page
 const homeBtn = document.getElementById("home")
-const aboutBtn = document.getElementById("about")
 const wrapper = document.querySelector('.wrapper');
 const registerLink = document.querySelector(".register-link")
-const about = document.querySelector(".about")
 const loginLink = document.querySelector(".login-link");
 const loginForm = document.getElementById("login-form")
 var person = [{ name: "admin", email: "admin@gmail.com", password: "admini" }, { name: "ardi", email: "admin@gmail.com", password: "123" }]
 // console.log(loginForm)
 var isLabelBtnClicked = false
-var isAboutBtnClicked = false
 var isLogBtnClicked = false //Ngarkimi i login page
 console.log(loginLink)
 
@@ -26,14 +21,14 @@ console.log(loginLink)
 function updateSectionVisibility() {
 
 
-const windowWidth = window.innerWidth;
-console.log(section)
-// console.log(windowWidth);
-console.log(loginLink.checked)
-if(!isLogBtnClicked){
-if (windowWidth <= 700) {
-    text.style.display = "none";
-    img.classList.add("active")
+    const windowWidth = window.innerWidth;
+    console.log(section)
+    console.log(windowWidth);
+    console.log(loginLink.checked)
+    if (!isLogBtnClicked) {
+        if (windowWidth <= 700) {
+            text.style.display = "none";
+            img.classList.add("active")
 
 
         } else {
@@ -46,20 +41,16 @@ if (windowWidth <= 700) {
         }
     }
 
-if(isLogBtnClicked){
-    if (windowWidth <= 1600) {
-        text.style.display = "none";
-        img.style.display = "none";
-        wrapper.classList.add("window")
+    if (isLogBtnClicked) {
+        if (windowWidth <= 1600) {
+            text.style.display = "none";
+            img.style.display = "none";
+            wrapper.classList.add("window")
+        }
+        else {
+            img.style.display = "block";
+        }
     }
-    else{
-        img.style.display = "block";
-    }
-}
-
-if(isAboutBtnClicked && windowWidth<=1600){
-    img.style.display = "none"
-}
 }
 
 //Thirret funksioni kur te  ngarkohet faqja 
@@ -124,41 +115,38 @@ loginLink.onclick = () => {
 
 
 //Ngarkimi i log faqes
-logBtn.addEventListener('click',()=>{
-about.classList.remove("about-active")
-isLogBtnClicked = true;
-console.log(isLogBtnClicked)
-btn.checked = false;
-text.style.display = "none";
-wrapper.classList.add("loging-active")
-img.classList.add("loging-active")
-updateSectionVisibility();
+logBtn.addEventListener('click', () => {
+    isLogBtnClicked = true;
+    console.log(isLogBtnClicked)
+    btn.checked = false;
+    text.style.display = "none";
+    wrapper.classList.add("loging-active")
+    img.classList.add("loging-active")
+    updateSectionVisibility();
 
 })
 
 
 //Trajtimi i Home button 
-homeBtn.addEventListener('click',()=>{
-about.classList.remove("about-active")
-isLogBtnClicked = false
- btn.checked = false;
- text.style.display ="block"
- img.style.display = "block"
- wrapper.classList.remove("loging-active")
- img.classList.remove("loging-active")
- updateSectionVisibility();
-//  console.log(wrapper.className == "loging-active")
-// isLogDisplayed = !isLogDisplayed;
-// console.log(isLogDisplayed)
+homeBtn.addEventListener('click', () => {
+    isLogBtnClicked = false
+    btn.checked = false;
+    text.style.display = "block"
+    img.style.display = "block"
+    wrapper.classList.remove("loging-active")
+    img.classList.remove("loging-active")
+    updateSectionVisibility();
+    //  console.log(wrapper.className == "loging-active")
+    // isLogDisplayed = !isLogDisplayed;
+    // console.log(isLogDisplayed)
 
 })
 
 
 //Kur deshirojme mu logu 
 function getLoginForm() {
-
-var username = document.querySelector(".username").value;
-var password = document.querySelector(".password").value;
+    var username = document.querySelector(".username").value;
+    var password = document.querySelector(".password").value;
 
 
     // console.log(username)
@@ -166,16 +154,15 @@ var password = document.querySelector(".password").value;
 
     if (isValid) {
 
-window.location.href = "courses.html"
-var n = document.getElementById("after")
-    n.style.background = "red"
-console.log(n)
-}
-else{
-    alert("Bad credentical")
-    document.getElementById("login-form").reset();
+        window.location.href = "courses.html"
+        var n = document.getElementById("after")
+        n.style.background = "red"
+        console.log(n)
+    }
+    else {
+        alert("Bad credentials")
+    }
 
-}
 }
 
 
@@ -201,41 +188,10 @@ function getSingUpForm() {
 
     var newPerson = new Object();
 
-newPerson.name = username;
-newPerson.email = email;
-newPerson.password = password;
-person.push(newPerson);
-console.log(newPerson)
-document.getElementById("register-form").reset();
-
+    newPerson.name = username;
+    newPerson.email = email;
+    newPerson.password = password;
+    person.push(newPerson);
+    console.log(newPerson)
 
 }
-
-var anchors = document.querySelectorAll('ul a');
-
-// Add click event listeners to each anchor element
-anchors.forEach(function(anchor) {
-    anchor.addEventListener('click', function() {
-        // Remove 'clicked' class from all anchors
-        anchors.forEach(function(a) {
-            a.classList.remove('clicked');
-        });
-
-        // Add 'clicked' class to the clicked anchor
-        this.classList.add('clicked');
-    });
-});
-
-
-
-
-aboutBtn.addEventListener('click',()=>{
-    isAboutBtnClicked =true
-    wrapper.classList.remove("loging-active")
-    console.log(isLogBtnClicked)
-    btn.checked = false;
-    text.style.display = "none";
-    mainImg.style.display = "none"    
-    console.log(img)
-    about.classList.add("about-active")
-    })
